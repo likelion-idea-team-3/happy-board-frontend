3008dfbda13ada9ba67fcf754da6e5fa11af7095
hyunsik2000 <ryuhsik2000@naver.com>
2024-06-09 20:42:44 +0900

    add: Route 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignupForm from './components/SignupForm/SignupForm';
+import MainPage from "./Page/MainPage/MainPage";
 
 const App = () => {
     return (
-        <div className="App">
-            <SignupForm />
-        </div>
+<>
+<Routes>
+  <Route exact path="/" element={<MainPage />} />
+  <Route path="/signup"element={<SignupForm />} />
+</Routes>
+</>
     );
 };
 
