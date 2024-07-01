31bbeb86fda6fa83e642452fa6d0e9396ded43af
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-01 19:12:18 +0900

    fix : 따옴표 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignupForm from './components/SignupForm/SignupForm';
-import MainPage from './Page/MainPage/MainPage';
-import Header from './SideComponent/Header/Header';
-import Footer from './SideComponent/Footer/Footer';
-import NonePage from './Page/NonePage/NonePage';
-import DetailPost from './Page/DetailPostPage/DetailPost';
-import MoreNews from './Page/MorePage/MoreNews';
-import LoginPage from './Page/TestLoginPage/LoginPage';
-import SignupPage from './Page/TestLoginPage/SignupPage';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignupForm from "./components/SignupForm/SignupForm";
+import MainPage from "./Page/MainPage/MainPage";
+import Header from "./SideComponent/Header/Header";
+import Footer from "./SideComponent/Footer/Footer";
+import NonePage from "./Page/NonePage/NonePage";
+import DetailPost from "./Page/DetailPostPage/DetailPost";
+import MoreNews from "./Page/MorePage/MoreNews";
+import LoginPage from "./Page/TestLoginPage/LoginPage";
+import SignupPage from "./Page/TestLoginPage/SignupPage";
 
 function App() {
     return (
