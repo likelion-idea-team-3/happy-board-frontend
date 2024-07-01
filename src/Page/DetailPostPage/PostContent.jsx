f2fc394bb58f2f350f7e2e9d6fbb5d7afc0fb936
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-07-01 18:37:53 +0900

    [fix] 게시판 정보 수정

--- src/Page/DetailPostPage/PostContent.jsx
+++ src/Page/DetailPostPage/PostContent.jsx
@@ -8,13 +8,10 @@ function PostContent() {
       <h1 className="post-title">제목</h1>
       <div className="post-detail-top">
         <span className="post-author">작성자</span>
-        <span className="post-info">조회수 123 | 10분 전</span>
+        <span className="post-info">10분 전</span>
       </div>
       <div className="post-detail-center">
         <div className="post-content">내용</div>
-      </div>
-      <div className="post-detail-bottom">
-        <FaRegHeart /> 7
       </div>
       </div>
   );
