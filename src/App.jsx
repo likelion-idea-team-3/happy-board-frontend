1adba3301b523a232e79d64e9d13bba922c0f835
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-07-01 18:38:10 +0900

    [add] 프록시 설정 추가 , api 테스트용 회원가입 / 로그인 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Header from "./SideComponent/Header/Header";
 import Footer from "./SideComponent/Footer/Footer";
 import NonePage from "./Page/NonePage/NonePage";
 import DetailPost from "./Page/DetailPostPage/DetailPost";
+import MoreNews from "./Page/MorePage/MoreNews";
+import LoginPage from "./Page/TestLoginPage/LoginPage";
+import SignupPage from "./Page/TestLoginPage/SignupPage";
 
 function App() {
   return (
@@ -15,7 +18,10 @@ function App() {
       <Routes>
         <Route exact path="/" element={<MainPage />} />
         <Route path="/signup" element={<SignupForm />} />
-        <Route path="/example" element={<DetailPost/>}/>
+        <Route path="/example" element={<DetailPost />} />
+        <Route path="/more" element={<MoreNews />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup1" element={<SignupPage/>} />
         <Route path="*" element={<NonePage/>} />
       </Routes>
       <Footer/>
---
+++ src/Page/TestLoginPage/LoginPage.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useAuth } from '../../SideComponent/Header/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8080/api/members/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
+      if (data.success) {
+        login({ email, name: data.data.name });
+        localStorage.setItem('userToken', data.data.token);
+        navigate('/');
+      } else {
+        console.error('로그인 오류:', data.message);
+      }
+    } catch (error) {
+      console.error('로그인 오류:', error);
+    }
+  };
+
+  return (
+    <div style={{margin:"200px"}}>
+      <h2>로그인</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          이메일:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </label>
+        <br />
+        <label>
+          비밀번호:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </label>
+        <br />
+        <button type="submit">로그인</button>
+        <button onClick={() => { navigate("/signup1")}}>회원가입</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
