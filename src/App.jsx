03cde4fb9e0aa9a4873a11a8b0c34526fc555093
hyunsik2000 <126768512+hyunsik2000@users.noreply.github.com>
2024-07-03 04:05:22 +0900

    충돌

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 import "./App.css";
 import React, { useEffect } from "react";
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-    useLocation,
-} from "react-router-dom";
-import SignupForm from "./components/SignupForm/SignupForm";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+
+import SignupForm from './components/SignupForm/SignupForm';
 import MainPage from "./Page/MainPage/MainPage";
+import LoginForm from './components/loginForm/LoginForm';
 import Header from "./SideComponent/Header/Header";
 import Footer from "./SideComponent/Footer/Footer";
 import NonePage from "./Page/NonePage/NonePage";
 import DetailPost from "./Page/DetailPostPage/DetailPost";
 import MoreNews from "./Page/MorePage/MoreNews";
-import LoginPage from "./Page/TestLoginPage/LoginPage";
 import SignupPage from "./Page/TestLoginPage/SignupPage";
 import MainPostBoard from "./Page/PostBoard/MainPostBoard";
 import EditArticle from "./Page/PostBoard/EditArticle";
@@ -38,7 +34,7 @@ function App() {
                 <Route exact path="/" element={<MainPage />} />
                 <Route path="/signup" element={<SignupForm />} />
                 <Route path="/more" element={<MoreNews />} />
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/login" element={<LoginForm />} />
                 <Route path="/signup1" element={<SignupPage />} />
                 <Route path="/post" element={<MainPostBoard />} />
                 <Route path="/post/:postId" element={<DetailPost />} />
