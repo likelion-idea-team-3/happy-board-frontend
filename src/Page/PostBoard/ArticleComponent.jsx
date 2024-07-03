32309823d3b682afe22f86521b1b16ac1178a9a2
minwoo1119 <84209687+minwoo1119@users.noreply.github.com>
2024-07-03 16:00:25 +0900

    fix : 컴포넌트 이미지 제거, 색상 띠 스타일 추가

--- src/Page/PostBoard/ArticleComponent.jsx
+++ src/Page/PostBoard/ArticleComponent.jsx
@@ -1,16 +1,13 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import BirdImg from "./testsrc/bird.jpeg";
 import "./ArticleComponent.css";
 
 function ArticleComponent(props) {
     return (
         <>
             <div className="articleComponent">
+                <div className="colorStripe"></div> {/* 상단 색상 띠 추가 */}
                 <Link to="#" style={{ textDecoration: "none" }}>
-                    <div className="imgbox">
-                        <img src={props.imgSrc} alt="article" />
-                    </div>
                     <div className="textbox">
                         <div className="textheader">{props.title}</div>
                         <div className="writer">{props.writer}</div>
@@ -33,14 +30,12 @@ function ArticleComponent(props) {
 }
 
 ArticleComponent.defaultProps = {
-    imgSrc: BirdImg, // 여기에 사용할 기본 이미지의 URL을 넣으세요
     showEditButton: false,
     onEdit: () => {},
     onDelete: () => {},
 };
 
 ArticleComponent.propTypes = {
-    imgSrc: PropTypes.string,
     title: PropTypes.string.isRequired,
     postedDay: PropTypes.string.isRequired,
     showEditButton: PropTypes.bool,
